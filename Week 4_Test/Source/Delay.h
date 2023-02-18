03fe17c0c54bbecec95ee43f214c3ccb4a5e8070
Jake Cheng <124639227+jakechen9@users.noreply.github.com>
2023-02-17 20:05:57 -0800

    test version

---
+++ Week 4_Test/Source/Delay.h
@@ -0,0 +1,20 @@
+/*
+  ==============================================================================
+
+    Delay.h
+    Created: 13 Feb 2023 5:12:54pm
+    Author:  程
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class Delay{
+public:
+    
+    void setSampleRate(float inSampleRate);
+    
+};
